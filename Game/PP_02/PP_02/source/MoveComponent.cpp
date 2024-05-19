1b561bddcb102864e0541c46978521cf814e8c4a
cr300002 <123cr0002@tokyo.amg.ac.jp>
2024-05-19 11:59:00 +0900

    処理の記述

--- Game/PP_02/PP_02/source/MoveComponent.cpp
+++ Game/PP_02/PP_02/source/MoveComponent.cpp
@@ -3,7 +3,7 @@
 #include <Math.h>
 MoveComponent::MoveComponent(class ObjectBase* owner, int updateowder) :Component(owner, updateowder)
 {
-	mPad = new GameXPad();
+
 }
 
 MoveComponent::~MoveComponent()
@@ -14,13 +14,5 @@ MoveComponent::~MoveComponent()
 void MoveComponent::Update()
 {
 	Component::Update();
-	mPad->Input();
-	Vector3D vec = Vector3D(0, 0, 0);
-	//キーに対応した数値を入れる
-	//if(mPad->GetXKey(XINPUT_BUTTON_A)) { vec._x + 10; }
-	//if(mPad->GetXKey(XINPUT_BUTTON_B)) { vec._y + 10; }
-	//if(mPad->GetXKey(XINPUT_BUTTON_X)) { vec._x - 10; }
-	//if(mPad->GetXKey(XINPUT_BUTTON_Y)) { vec._y - 10; }
-	//位置情報の更新
-	mOwner->SetPos(vec);
+	
 }
--- Game/PP_02/PP_02/source/MoveComponent.h
+++ Game/PP_02/PP_02/source/MoveComponent.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "Component.h"
-#include "GameXPad.h"
 class MoveComponent : public Component
 {
 public:
-	MoveComponent(class ObjectBase* owner, int updateowder = 100);
+	MoveComponent(class ObjectBase* owner, int updateowder = 10);
 	virtual ~MoveComponent();
 	//更新
 	void Update()override;
+	//ゲッター
+	float GetAngularSpeed() { return mAngularSpeed; }
+	float GetForwardSpeed() { return mForwardSpeed; }
+	//セッター
+	void SetAngularSpeed(float speed) { mAngularSpeed = speed; }
+	void SetForwardSpeed(float speed) { mForwardSpeed = speed; }
 protected:
-	GameXPad* mPad;
-
+	//回転制御(ラジアン/秒)
+	float mAngularSpeed;
+	//前進運動制御(単位長/秒)
+	float mForwardSpeed;
 };
 
