3854242065ca61690ba3229116b4e0a1a68502f4
fuky <123cr0002@tokyo.amg.ac.jp>
2024-05-24 16:18:32 +0900

    AIに攻撃の状態追加

---
+++ Game/PP_02/PP_02/source/AIAttack.cpp
@@ -0,0 +1,21 @@
+#include "AIAttack.h"
+
+AIAttack::AIAttack(AIComponent* owner)
+{
+}
+
+AIAttack::~AIAttack()
+{
+}
+
+void AIAttack::Update(float deltatime)
+{
+}
+
+void AIAttack::OnEnter()
+{
+}
+
+void AIAttack::OnExit()
+{
+}
