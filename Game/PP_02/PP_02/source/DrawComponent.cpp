f58e61ab4c375b200e99132318e4ed0b76ee13b8
cr300002 <123cr0002@tokyo.amg.ac.jp>
2024-05-27 14:25:14 +0900

    処理の記述

--- Game/PP_02/PP_02/source/DrawComponent.cpp
+++ Game/PP_02/PP_02/source/DrawComponent.cpp
@@ -1,1 +1,17 @@
 #include "DrawComponent.h"
+#include "DxLib.h"
+
+DrawComponent::DrawComponent(ObjectBase* owner, int updateowder) :Component(owner, updateowder)
+{
+	mPass = "";
+	mHandle = MV1LoadModel((const TCHAR*)(mPass.c_str()));
+}
+
+DrawComponent::~DrawComponent()
+{
+}
+
+void DrawComponent::Update(float deltatime)
+{
+	MV1DrawModel(mHandle);
+}
--- Game/PP_02/PP_02/source/DrawComponent.h
+++ Game/PP_02/PP_02/source/DrawComponent.h
@@ -2,5 +2,15 @@
 #include "Component.h"
 class DrawComponent :public Component
 {
+public:
+	DrawComponent(class ObjectBase* owner, int updateowder = 100);
+	virtual ~DrawComponent();
+	//更新
+	void Update(float deltatime)override;
+	//ゲッター
+	int GetHandle() const { return mHandle; }
+protected:
+	int mHandle;
+	std::string mPass;
 };
 
