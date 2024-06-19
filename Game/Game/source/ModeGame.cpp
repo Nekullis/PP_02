9191c04dcf8987d6eaa4ffc4541feab8648cf34e
cr300002 <123cr0002@tokyo.amg.ac.jp>
2024-06-19 18:41:12 +0900

    敵の数を減らす

--- Game/Game/source/ModeGame.cpp
+++ Game/Game/source/ModeGame.cpp
@@ -6,6 +6,7 @@ bool ModeGame::Initialize()
 {
 	if (!ModeBase::Initialize()) { return false; }
 	//オブジェクトマネージャー初期化
+	
 	mObjManager = new ObjectManager();
 	mGameCol = new GameCollision();
 	mColManager = new CollisionManager();
@@ -27,18 +28,18 @@ void ModeGame::Register()
 {
 	Player* player = new Player(this);
 	EnemyMob* mob_l = new EnemyMob(this);
-	mob_l->SetPos(Vector3D(-100, 100, 1000));
-	EnemyMob* mob_r = new EnemyMob(this);
-	mob_r->SetPos(Vector3D(100, 100, 1000));
+	//mob_l->SetPos(Vector3D(-500, 100, 1000));
+	/*EnemyMob* mob_r = new EnemyMob(this);
+	mob_r->SetPos(Vector3D(100, 100, 1000));*/
 	Stage* stage = new Stage(this);
 }
 
 bool ModeGame::Process()
 {
 	ModeBase::Process();
+	mColManager->Update();
 	mObjManager->Process();
 	mGameCol->Update();
-	mColManager->Update();
 	return true;
 }
 
